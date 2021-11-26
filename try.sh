98aa74216b737cd3c2135175bf3d214b646368d4
dizdaz <javaraja@protonmail.com>
2021-11-26 12:54:17 +0330

    Minor changes.

--- try.sh
+++ try.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-comm="ls somefile"
+comm="ls"
 runs=5
 counter=1
 rest=3
 
 
-while [ $counter -le $runs ]; do
-  printf "\rAttemp $counter ..."
+while true; do
+  printf "\rAttempt $counter of $runs ..."
   out_p=$($comm 2>&1) #Capture stdout and stderr
   exitcode=$? #Capture the exit code
 
   #Exit with code zero if successful
   if [ $exitcode -eq 0 ]; then
     echo
-    echo "Success --> $out_p"
+    echo "$out_p"
     exit 0
 
   #Print out the error message if failed at the last attempt
